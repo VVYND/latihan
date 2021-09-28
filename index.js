4ed4d1b8d8f0cd198778346f81b139e052087081
VVYND <ramadewantara97@gmail.com>
2021-09-28 23:03:38 +0800

    testing

--- index.js
+++ index.js
@@ -77,6 +77,5 @@ app.post('/api/notes', (request,response) => {
 
   response.json(note)
 })
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`server run on ${PORT}`))
